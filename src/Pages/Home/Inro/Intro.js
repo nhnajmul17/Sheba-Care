14bf2fe5e94d2dc1f29f203d279b90cf2de639af
nhnajmul17 <nhnajmul17@gmail.com>
2021-10-19 19:40:42 +0600

    hover final

--- src/Pages/Home/Inro/Intro.js
+++ src/Pages/Home/Inro/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import img from '../../../Images/doctor.png'
+import './Intro.css'
 const Intro = () => {
     return (
         <div className='mt-5 container'>
@@ -8,7 +9,7 @@ const Intro = () => {
                     <h2 className='fs-1'>Welcome to <span className='text-success'>Sheba</span> <span className='header'>Care</span> </h2>
                     <p className='text-secondary'>Sheba Care is a private hospital established with a particularly strong presence of doctors in   Bangladesh. Our first facility was established with approximately 50 operational beds. The hospital now features over 500 operational beds through a combination of greenfield projects and acquisitions. We believe that the "<span className='text-success'>Sheba</span> <span className='header'>Care</span>" brand is strongly associated with our mission to deliver high-quality, affordable healthcare services to the broader population by leveraging our economies of scale, skilled doctors, and an efficient business model.</p>
                 </div>
-                <div className='col-lg-4 col-sm-12'>
+                <div className='col-lg-4 col-sm-12 doctor-img'>
                     <img style={{
                         width: '400px',
                         height: '350px'
--- src/Pages/Home/Services/Service/Service.js
+++ src/Pages/Home/Services/Service/Service.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Service.css'
 
 const Service = (props) => {
     const { name, picture, category, _id } = props.service;
@@ -9,21 +10,28 @@ const Service = (props) => {
         <div>
 
             <Col>
-                <Card>
-                    <div className='ps-4 '>
-                        <Card.Img style={{
-                            height: '100px',
-                            width: '100px'
-                        }} variant="top" src={picture} />
-                    </div>
-                    <Card.Body>
-                        <Card.Title className='text-secondary'>Department of {name}</Card.Title>
-                        <Card.Text> Service Type: {category} </Card.Text>
-                        <Link to={`/details/${_id}`}>
-                            <Button variant="primary">Get Details</Button>
-                        </Link>
-                    </Card.Body>
-                </Card>
+                <div className='card'>
+
+                    <Card>
+                        <div className='ps-4 img '>
+                            <Card.Img style={{
+                                height: '100px',
+                                width: '100px'
+                            }} variant="top" src={picture} />
+                        </div>
+                        <Card.Body>
+                            <Card.Title className='text-secondary'>Department of {name}</Card.Title>
+                            <Card.Text> Service Type: {category} </Card.Text>
+
+                            <Link to={`/details/${_id}`}>
+                                <Button variant="primary">Get Details</Button>
+                            </Link>
+
+
+                        </Card.Body>
+                    </Card>
+                </div>
+
             </Col>
         </div>
     );
